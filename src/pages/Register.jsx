f98a63d98c57bf5f80078baf4ee34b8f09d998eb
sdgsnehal <sdgsnehal@gmail.com>
2023-07-10 18:01:19 +0530

    Feat: Router added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import avatar from "../assets/addAvatar.png";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import {auth , storage} from "../firebase";
+import { createUserWithEmailAndPassword , updateProfile} from "firebase/auth";
+import {auth , storage,db} from "../firebase";
 import {  ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-
+import { doc, setDoc } from "firebase/firestore"; 
+import { useNavigate } from "react-router-dom";
 
 
 const Register = () => {
   const [err,setErr] = useState(false);
+  const naviagte =useNavigate()
   const handleSubmit =async(event)=>{
     event.preventDefault();
     const displayName =event.target[0].value;
@@ -28,32 +30,35 @@ const uploadTask = uploadBytesResumable(storageRef, file);
 // 1. 'state_changed' observer, called any time the state changes
 // 2. Error observer, called on failure
 // 3. Completion observer, called on successful completion
-uploadTask.on('state_changed', 
-  (snapshot) => {
-    // Observe state change events such as progress, pause, and resume
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is ' + progress + '% done');
-    switch (snapshot.state) {
-      case 'paused':
-        console.log('Upload is paused');
-        break;
-      case 'running':
-        console.log('Upload is running');
-        break;
-    }
-  }, 
+uploadTask.on(
+  
+    
+   
   (error) => {
     // Handle unsuccessful uploads
+    setErr(true);
   }, 
   () => {
     // Handle successful uploads on complete
     // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      console.log('File available at', downloadURL);
+    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+      await updateProfile(res.user,{
+        displayName,
+        photoURL:downloadURL,
+      })
+      await setDoc(doc(db ,"users",res.user.uid),{
+        uid:res.user.uid,
+        displayName, email, photoURL:downloadURL,
+      
+       });
+       await setDoc(doc(db,"userChats",res.user.uid),{})
+       naviagte("/")
     });
   }
 );
+ 
+
+
 
 }catch(err){
 setErr(true)
