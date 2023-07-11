fff152f97317a51633ee54fc4490c37534614475
sdgsnehal <sdgsnehal@gmail.com>
2023-07-11 12:29:22 +0530

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { useContext } from "react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./style.scss" 
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter,Routes,Route, Navigate } from "react-router-dom";
+import { AuthContext } from "./context/Authcontext";
+
 
 
 function App() {
+  const {currentUser} = useContext(AuthContext)
+  const ProtectedRoute = ({children})=>{
+    if(!currentUser){
+      return <Navigate  to="/login"/>
+    }
+
+}
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/">
-        <Route index element={<Home/>}/> 
+        <Route index element={<ProtectedRoute><Home/></ProtectedRoute>}/> 
         <Route path = "login" element={<Login/>}/>
         <Route path ="register" element = {<Register/>}/>
       </Route>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {signOut} from "firebase/auth"
+import { auth } from '../firebase'
 
 const Navbar = () => {
   return (
@@ -7,7 +9,7 @@ const Navbar = () => {
       <div className='user'>
         <img src="https://images.pexels.com/photos/14191240/pexels-photo-14191240.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
         <span>John</span>
-        <button>Logout</button>
+        <button onClick={()=>signOut(auth)}>Logout</button>
       </div>
     </div>
   )
